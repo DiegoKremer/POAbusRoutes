ee0d68944166483357f0a8fdf43fe9db88d9636e
Diego Kremer <diego.kremer@gmail.com>
2015-06-24 09:06:46 -0300

    Working on Edge

--- POAbusRoutes/src/graph/Edge.java
+++ POAbusRoutes/src/graph/Edge.java
@@ -1,8 +1,40 @@
 package graph;
 
 public class Edge {
+	
+	private int originVertice;
+	private int destinyVertice;
+	private int cost;
 
-	public Edge () {
-		
+	public Edge (int originVertice, int destinyVertice, int cost) {
+		this.originVertice = originVertice;
+		this.destinyVertice = destinyVertice;
+		this.cost = cost;
 	}
+
+	public int getOriginVertice() {
+		return originVertice;
+	}
+
+	public void setOriginVertice(int originVertice) {
+		this.originVertice = originVertice;
+	}
+
+	public int getDestinyVertice() {
+		return destinyVertice;
+	}
+
+	public void setDestinyVertice(int destinyVertice) {
+		this.destinyVertice = destinyVertice;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	
+	
 }
