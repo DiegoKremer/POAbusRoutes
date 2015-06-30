147565fe0c514e008b53737d50115752f6dad82e
DiegoKremer <diego.kremer@gmail.com>
2015-06-30 11:24:52 -0300

    Updating Dijkstra algorithm

--- POAbusRoutes/src/graph/Graph.java
+++ POAbusRoutes/src/graph/Graph.java
@@ -37,21 +37,21 @@ public class Graph {
 		Vector<Boolean> visited = new Vector<Boolean>();
 		visited.set(originIndex, false);
 		
-		Vector<Vertex> adjacents = new Vector<Vertex>();
+		Vector<Edge> adjacents = new Vector<Edge>();
 		
 		/* Initialize Costs*/
 		
-		Vector<Integer> costs = new Vector<Integer> ();
+		Vector<Double> costs = new Vector<Double> ();
 		
 		for (int i = 0; i < vertices.size(); i++) {
-			costs.add(i, Integer.MAX_VALUE);
+			costs.add(i, Double.MAX_VALUE);
 		}
 		
 		/* Initialize Previous */
 		Vector<Vertex> previous = new Vector<Vertex> ();
 		
 		/* Cost vector origin point starts with 0 */
-		costs.add(originIndex, 0);
+		costs.add(originIndex, 0.0);
 		
 		stack.add(vertices.get(originIndex));
 		while (!stack.isEmpty()) {
@@ -64,13 +64,28 @@ public class Graph {
 				/* Get adjacents vertices */
 				for (int edgesIndex = 0;  edgesIndex < edges.size(); edgesIndex++) {
 					if (edges.get(edgesIndex).getDestinyVertex().equals(element)) {
-						adjacents.add(edges.get(edgesIndex).getDestinyVertex());
+						adjacents.add(edges.get(edgesIndex));
 					}
 				}
-				
+				/* Para cada vértice (v) vizinho de n: */
 				for (int adjacentsIndex = 0; adjacentsIndex < adjacents.size(); adjacentsIndex++) {
-					
+					/* custo = custos[n] + custo(n,v) */
+					double cost = costs.get(originIndex) + adjacents.get(adjacentsIndex).getCost();
+					/* Se custo < custos(v): */
+					if (cost < costs.get(adjacents.get(adjacentsIndex).getDestinyVertex().getIndex())) {
+						/* custos[v] = custo */
+						costs.add(adjacents.get(adjacentsIndex).getDestinyVertex().getIndex(), cost);
+						/* anterior[v] = n  */
+						previous.add(adjacents.get(adjacentsIndex).getDestinyVertex().getIndex(), element);
+					}
+					/* Se não visitou v */
+					if (visited.get(adjacents.get(adjacentsIndex).getDestinyVertex().getIndex()) == false) {
+						/*  adiciona v à Pilha */
+						stack.push(adjacents.get(adjacentsIndex).getDestinyVertex());
+					}
 				}
+				/* Marca n como visitado */
+				visited.add(adjacents.get(adjacentsIndex).getDestinyVertex().getIndex(), true);
 				
 				
 			}
